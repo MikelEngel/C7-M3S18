fb34fe0865bb660971c57257a78b5ebe2d09b0f2
MikelEngel <angelhevel@gmail.com>
2022-09-16 23:09:20 -0500

    update

--- main.js
+++ main.js
@@ -24,7 +24,35 @@ const mascotas = [
         nombre: "Michi"
 
     },
+
+    {
+        tipo: "Perro",
+        edad: 5,
+        color: "Negro",
+        peso: "8 kg",
+        nombre: "Cuco"
+
+    },
+
+    {
+        tipo: "Gato",
+        edad: 2,
+        color: "Marrón",
+        peso: "3 kg",
+        nombre: "Felix"
+
+    },
+
 ]
+const peliculas = [
+    {
+        tipo: "Rápido y furioso",
+        year: "2002",
+        genero: "accion",
+        precio: 20
+    }
+]
+
  
 //! 1. Importar tods nuestras bibliotecas
 
@@ -58,6 +86,58 @@ app.get ("/mascota", (req, res)=> {
     res.json({ mascotas });
 });
 
+//CREATE - Se hace con POST PELICULAS
+app.post("/pelicula", (req, res)=>{
+    const { tipo, year, genero, precio } = req.body;
+    peliculas.push( {tipo, year, genero, precio});
+    res.jason({mensaje: "Pelicula agregada", data:peliculas});
+});
+
+//READ - GET
+app.get ("/pelicula", (req, res)=> {
+    res.json({ peliculas });
+});
+
+
+//UPDATE - PUT
+
+app.put("/mascota", (req, res)=> {
+   /*  let indice = -1;
+    //Buscar y editar.
+    for(let i = 0; i<mascotas.length; i++){
+        if (mascotas [i].nombre === req.params.nombre){
+            indice=i;
+        }
+    } */
+
+    const indice = mascotas.findIndex ((mascota)=> mascota.nombre === req.params.nombre);
+
+    if (indice === -1){
+        res.jason({error: 'No se encontro a la mascota'});
+
+    }
+
+    const{
+        tipo,
+        edad,
+        color,
+        peso,
+        nombre,
+    } =req.body;
+    mascotas[indice] = {
+        ...mascotas[indice],
+        ...{
+            tipo,
+            edad,
+            color,
+            peso,
+            nombre,  
+        },
+    };
+
+    res.json({ mascotaEditada: mascotas[indice], todasMascotas: mascotas });
+
+});
 
 //! 5. Levantar el servidor
 
